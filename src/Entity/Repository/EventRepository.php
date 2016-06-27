825c07ee63892a8281fe5f8a3c31fdbe624cb73b
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2016-06-27 15:49:54 +0200

    Event repository docblocks.

--- src/Entity/Repository/EventRepository.php
+++ src/Entity/Repository/EventRepository.php
@@ -13,47 +13,84 @@ namespace TheSportsDb\Entity\Repository;
  */
 class EventRepository extends Repository implements EventRepositoryInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   protected $entityType = 'event';
+
+  /**
+   * {@inheritdoc}
+   */
   public function byName($name) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchevents.php', array('e' => $name))->event);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byFileName($fileName) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchfilename.php', array('e' => $fileName))->event);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byNameAndSeason($name, $season) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchevents.php', array('e' => $name, 's' => $season))->event);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function nextFiveByTeam($teamId) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsnext.php', array('id' => $teamId))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function nextFifteenEventsByLeague($leagueId) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsnextleague.php', array('id' => $leagueId))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function nextFifteenEventsByLeagueAndRound($leagueId, $round) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsnextleague.php', array('id' => $leagueId, 'r' => $round))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function lastFiveByTeam($teamId) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventslast.php', array('id' => $teamId))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function lastFifteenEventsByLeague($leagueId) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventspastleague.php', array('id' => $leagueId))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byDay(\DateTime $date, $sport = NULL, $leagueName = NULL) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsday.php', array('d' => $date->format('Y-m-d'), 's' => $sport, 'l' => $leagueName))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byLeagueRoundAndSeason($leagueId, $round, $season) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsround.php', array('id' => $leagueId, 'r' => $round, 's' => $season))->events);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byLeagueAndSeason($leagueId, $season) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('eventsseason.php', array('id' => $leagueId, 's' => $season))->events);
   }
--- src/Entity/Repository/EventRepositoryInterface.php
+++ src/Entity/Repository/EventRepositoryInterface.php
@@ -12,15 +12,139 @@ namespace TheSportsDb\Entity\Repository;
  * @author Jelle Sebreghts
  */
 interface EventRepositoryInterface extends RepositoryInterface {
+
+  /**
+   * Load events by name.
+   *
+   * @param string $name
+   *   The name of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byName($name);
+
+  /**
+   * Load events by filename.
+   *
+   * @param string $fileName
+   *   The file name of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byFileName($fileName);
+
+  /**
+   * Load events by name and season.
+   *
+   * @param string $name
+   *   The name of the event.
+   * @param string $season
+   *   The season of the event
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byNameAndSeason($name, $season);
+
+  /**
+   * Load events by day, sport and league.
+   *
+   * @param \DateTime $date
+   *   The date of the event.
+   * @param string $sport
+   *   The sport of the event.
+   * @param string $leagueName
+   *   The name of the league of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byDay(\DateTime $date, $sport = NULL, $leagueName = NULL);
+
+  /**
+   * Get the next five events for a team.
+   *
+   * @param string $teamId
+   *   The id of the team.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function nextFiveByTeam($teamId);
+
+  /**
+   * Load the next fifteen events by league.
+   *
+   * @param string $leagueId
+   *   The id of the league of the events.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function nextFifteenEventsByLeague($leagueId);
+
+  /**
+   * Load the next fifteen events by league and round.
+   *
+   * @param string $leagueId
+   *   The league id of the league of the event.
+   * @param string round
+   *   The round of the events.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function nextFifteenEventsByLeagueAndRound($leagueId, $round);
+
+  /**
+   * Load the last five events by team.
+   *
+   * @param string $teamId
+   *   The id of the teams of the events.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function lastFiveByTeam($teamId);
+
+  /**
+   * Load the last fifteen events by league.
+   *
+   * @param string $leagueId
+   *   The league id of the league of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function lastFifteenEventsByLeague($leagueId);
+
+  /**
+   * Load events by league, round and season.
+   *
+   * @param string $leagueId
+   *   The league id of the league of the event.
+   * @param string $round
+   *   The round of the event.
+   * @param string $season
+   *   The season of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byLeagueRoundAndSeason($leagueId, $round, $season);
+
+  /**
+   * Load events by league and season.
+   *
+   * @param string $leagueId
+   *   The league id of the league of the event.
+   * @param string $season
+   *   The season of the event.
+   *
+   * @return \TheSportsDb\Entity\EventInterface[]
+   *   An array of matched events.
+   */
   public function byLeagueAndSeason($leagueId, $season);
 }
