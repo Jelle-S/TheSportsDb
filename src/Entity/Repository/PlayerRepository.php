29b2fd98ca0facd71bd6f5892fc0a10caaaa9912
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2016-06-27 16:00:59 +0200

    Player repository docblocks.

--- src/Entity/Repository/PlayerRepository.php
+++ src/Entity/Repository/PlayerRepository.php
@@ -13,20 +13,35 @@ namespace TheSportsDb\Entity\Repository;
  */
 class PlayerRepository extends Repository implements PlayerRepositoryInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   protected $entityType = 'player';
 
+  /**
+   * {@inheritdoc}
+   */
   public function byTeamName($name) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchplayers.php', array('t' => $name))->player);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byName($name) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchplayers.php', array('p' => $name))->player);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byTeamNameAndName($teamName, $name) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('searchplayers.php', array('p' => $name, 't' => $teamName))->player);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function byTeam($teamId) {
     return $this->normalizeArray($this->sportsDbClient->doRequest('lookup_all_players.php', array('id' => $teamId))->player);
   }
--- src/Entity/Repository/PlayerRepositoryInterface.php
+++ src/Entity/Repository/PlayerRepositoryInterface.php
@@ -13,11 +13,49 @@ namespace TheSportsDb\Entity\Repository;
  */
 interface PlayerRepositoryInterface extends RepositoryInterface {
 
+  /**
+   * Get players by team.
+   *
+   * @param string $teamId
+   *   The id of the team of the player.
+   *
+   * @return \TheSportsDb\Entity\PlayerInterface[]
+   *   The matched players.
+   */
   public function byTeam($teamId);
 
+  /**
+   * Get players by team name.
+   *
+   * @param string $teamName
+   *   The name of the team of the player.
+   *
+   * @return \TheSportsDb\Entity\PlayerInterface[]
+   *   The matched players.
+   */
   public function byTeamName($name);
 
+  /**
+   * Get players by name.
+   *
+   * @param string $name
+   *   The name of the player.
+   *
+   * @return \TheSportsDb\Entity\PlayerInterface[]
+   *   The matched players.
+   */
   public function byName($name);
 
+  /**
+   * Get players by team name and name.
+   *
+   * @param string $teamName
+   *   The id of the team of the player.
+   * @param string $name
+   *   The name of the player.
+   *
+   * @return \TheSportsDb\Entity\PlayerInterface[]
+   *   The matched players.
+   */
   public function byTeamNameAndName($teamName, $name);
 }
